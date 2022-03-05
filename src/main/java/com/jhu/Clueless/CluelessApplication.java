8f9059b1d145edbb17f691c0c6ffa6645e8e4a87
kzn456 <52165910+kzn456@users.noreply.github.com>
2022-03-04 22:30:12 -0500

    Add files via upload

---
+++ src/main/java/com/jhu/Clueless/CluelessApplication.java
@@ -0,0 +1,13 @@
+package com.jhu.Clueless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CluelessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CluelessApplication.class, args);
+	}
+
+}
